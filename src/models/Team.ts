fb960c56852627e0fdef1e43351ddd2c885e978c
Jamesk1281 <26jkouvlis@xbhs.net>
2026-01-08 09:21:34 -0500

    Add fromSeed method on Team to allow initialization of Team items from TeamSeed items

--- src/models/Team.ts
+++ src/models/Team.ts
@@ -1,18 +1,30 @@
 import { Player } from "./Player";
+import type { TeamSeed } from "../data/TeamSeed"
 
 export class Team {
   readonly id: string;
   name: string;
+  abbreviation: string;
 
   roster: Player[];
 
   constructor(params: {
     id: string;
     name: string;
+    abbreviation: string;
     roster: Player[];
   }) {
     this.id = params.id;
     this.name = params.name;
+    this.abbreviation = params.abbreviation;
     this.roster = params.roster;
+  }
+
+  static fromSeed(seed: TeamSeed): Team {
+    return new Team({
+      id: seed.id,
+      name: seed.name,
+      roster: []
+    })
   }
 }
